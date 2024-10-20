abb6b49f1494cc8edae00be6144c896b3748a0af
Rassulkhair <rassulkhairshokanov@gmail.com>
2024-10-20 20:47:53 +0500

    Прописан метод printAllBooks

--- src/ArraysEngines.java
+++ src/ArraysEngines.java
@@ -5,36 +5,40 @@ public class ArraysEngines {
     // Книги
     // - <название_книги>: <автор> <кол-во страниц> стр
     public void printAllBooks(Book[] books) {
-
+        for (Book book : books) {
+            if (book != null) {
+                System.out.println("Название книги: " + book.title + " | Автор книги: " + book.author + " | Количество страниц: " + book.pages);
+            }
+        }
     }
 
 
     // TODO: метод должен посчитать фактическое кол-во книг, не учитывать null
     public int getActualSize(Book[] books) {
-
+        return 0;
     }
 
 
     // TODO: метод должен посчитать общее количество страниц всех книг массива
     public int countTotalPages(Book[] books) {
-
+        return 0;
     }
 
 
     // TODO: метод должен возвращать объект книги из массива по переданному название
     public Book findBookByTitle(Book[] books, String title) {
-
+        return null;
     }
 
 
     // TODO: метод должен возвращать массив строк, которая состоит только из названия книг
     public String[] getBookTitles(Book[] books) {
-
+        return null;
     }
 
 
     // TODO: метод должен находить книгу с самой наибольшей страницей
     public Book findLargestBook(Book[] books) {
-
+        return null;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Book book = new Book("Преступление и наказание", "Федор Достоевский", 458);
+        Book[] books = new Book[3];
+        Book book1 = new Book("Преступление и наказание", "Федор Достоевский", 550);
+        Book book2 = new Book("Степной волк", "Герман Гессе", 250);
+
+        books[0] = book1;
+        books[1] = book2;
+        ArraysEngines arrays = new ArraysEngines();
+        arrays.printAllBooks(books);
     }
 }
